f9a98ed8bc9f48ca3ee8568b3feb22b047dbd526
DavLaw1 <kingx5926@gmail.com>
2022-05-20 10:31:58 -0400

    table creation and mock data

---
+++ social-mock-data.sql
@@ -0,0 +1,17 @@
+set search_path to social_app;
+
+insert into app_users (email, age, username, password)
+values
+    ('washed@gmail.com', 19, 'slimed2l4', 'passlol'),
+    ('saneman@gmail.com', 19, 'wispher', 'pass231'),
+    ('lmsfh@gmail.com', 19, 'dfgh544', 'wpasses'),
+    ('hasmey@gmail.com', 19, 'dontplayhaha', 'nopass'),
+    ('pastlife@gmail.com', 19, 'imaliveagain', 'imasspass'),
+    ('mickel@gmail.com', 19, 'yessir', 'passed'),
+    ('flyingsq@gmail.com', 19, 'icanfly', 'plolpas'),
+    ('monkguy@gmail.com', 19, 'monkperson', 'mypass'),
+    ('spiderman@gmail.com', 19, 'ishootwebs', 'yourpass'),
+    ('animewatcher@gmail.com', 19, 'myheroaca', 'ourpass'),
+    ('iplayleague@gmail.com', 19, 'nolife123', 'thepass'),
+    ('crafters@gmail.com', 19, 'mcisawesome', 'hepassed'),
+    ('ssundee@gmail.com', 19, 'derp', 'stopthepass');
